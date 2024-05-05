cc889dece71f178f9afbb73ffaa7d70370a5b25b
quwenjie <2382036672@qq.com>
2024-05-06 03:34:14 +0800

    support _int128, 2^80 commit

--- src/hyrax.cpp
+++ src/hyrax.cpp
@@ -7,10 +7,10 @@ using namespace mcl::bn;
 const int MAX_MSM_LEN=1e4;
 const int COMM_OPT_MAX=65536; //don't optimize if larger than this
 const int logmax=16;  /// max number=2^18-1
-const int block_num=4;
+const int block_num=5;  //2^80
 
 
-inline G1 perdersen_commit(G1* g,ll* f,int n,G1* W)
+G1 perdersen_commit(G1* g,ll* f,int n,G1* W)
 {
     G1 ret;
     ret.clear();
@@ -33,9 +33,6 @@ inline G1 perdersen_commit(G1* g,ll* f,int n,G1* W)
                     W[fnow+(j<<logmax)]-=g[i];
                     used[fnow+(j<<logmax)]=1;
                 }
-                //W[-f[i]]-=g[i];
-                //used[-f[i]]=1;
-                //assert(-f[i]<COMM_OPT_MAX);
             }
             else
             {
@@ -46,13 +43,8 @@ inline G1 perdersen_commit(G1* g,ll* f,int n,G1* W)
                     W[fnow+(j<<logmax)]+=g[i];
                     used[fnow+(j<<logmax)]=1;
                 }
-                //W[f[i]]+=g[i];
-                //used[f[i]]=1;
-                //assert(f[i]<COMM_OPT_MAX);
             }
     }
-    //t.stop("add ",false);
-    //const int logn=log2(COMM_OPT_MAX)+1;
     G1 gg[logmax*block_num];
     for(int j=0;j<logmax*block_num;j++)
         gg[j].clear();
@@ -71,16 +63,22 @@ inline G1 perdersen_commit(G1* g,ll* f,int n,G1* W)
             used[j]=0;            
         }
     }
-    for(int j=0;j<logmax*4;j++)
-        ret+=gg[j]*(1ll<<j);
-    //t.stop("accu",false);
-    //t.stop("ALL: ",true);
+    for(int j=0;j<logmax*block_num;j++)
+    {
+        if(j>60)
+        {
+            G1 gd=gg[j]*(1ll<<48);
+            ret+=gd*(1ll<<(j-48)); //split
+        }
+        else
+            ret+=gg[j]*(1ll<<j);
+    }
     delete []used;
     return ret;
 }
 
 
-inline G1 perdersen_commit(G1* g,int* f,int n,G1* W)
+G1 perdersen_commit(G1* g,int* f,int n,G1* W)
 {
     G1 ret;
     ret.clear();
@@ -104,7 +102,7 @@ inline G1 perdersen_commit(G1* g,int* f,int n,G1* W)
                 assert(f[i]<COMM_OPT_MAX);
             }
     }
-    t.stop("add ",false);
+    //t.stop("add ",false);
     const int logn=log2(COMM_OPT_MAX)+1;
     G1 gg[40];
     for(int j=0;j<logn;j++)
@@ -132,76 +130,7 @@ inline G1 perdersen_commit(G1* g,int* f,int n,G1* W)
     return ret;
 }
 
-inline G1 perdersen_commit(G1* g,ll* f,int n,G1* W)
-{
-    G1 ret;
-    ret.clear();
-    //timer t(true);
-    //t.start();
-    
-    bool *used=new bool[COMM_OPT_MAX*block_num];
-    memset(used,0,sizeof(bool)*COMM_OPT_MAX*block_num);
-    for(int i=0;i<n;i++)
-    {
-            if(f[i]==0)
-                continue;
-            
-            if(f[i]<0)
-            {
-                ll tmp=-f[i];
-                for(int j=0;j<block_num;j++)
-                {
-                    ll fnow=(tmp>>(logmax*j))&65535;
-                    W[fnow+(j<<logmax)]-=g[i];
-                    used[fnow+(j<<logmax)]=1;
-                }
-                //W[-f[i]]-=g[i];
-                //used[-f[i]]=1;
-                //assert(-f[i]<COMM_OPT_MAX);
-            }
-            else
-            {
-                ll tmp=f[i];
-                for(int j=0;j<block_num;j++)
-                {
-                    ll fnow=(tmp>>(logmax*j))&65535;
-                    W[fnow+(j<<logmax)]+=g[i];
-                    used[fnow+(j<<logmax)]=1;
-                }
-                //W[f[i]]+=g[i];
-                //used[f[i]]=1;
-                //assert(f[i]<COMM_OPT_MAX);
-            }
-    }
-    //t.stop("add ",false);
-    //const int logn=log2(COMM_OPT_MAX)+1;
-    G1 gg[logmax*block_num];
-    for(int j=0;j<logmax*block_num;j++)
-        gg[j].clear();
-    for(int j=0;j<COMM_OPT_MAX*block_num;j++)
-    {
-        if(used[j])
-        {
-            int jj=j%COMM_OPT_MAX;
-            int blk=j/COMM_OPT_MAX;
-            for(int k=0;k<logmax;k++)
-            {
-                if(jj&(1<<k))
-                    gg[k+logmax*blk]+=W[j];
-            }
-            W[j].clear();
-            used[j]=0;            
-        }
-    }
-    for(int j=0;j<logmax*4;j++)
-        ret+=gg[j]*(1ll<<j);
-    //t.stop("accu",false);
-    //t.stop("ALL: ",true);
-
-    delete []used;
-    return ret;
-}
-inline G1 perdersen_commit(G1* g,Fr* f,int n)
+G1 perdersen_commit(G1* g,Fr* f,int n)
 {
     G1 ret;
     G1::mulVec(ret,g,f,n);
@@ -413,8 +342,10 @@ G1* prover_commit(ll* w, G1* g, int l,int thread_n) //compute Tk, int version wi
     timer t;
     t.start();
     G1** W=new G1*[thread_n];
+    cout<<"start allocate mem "<<endl;
     for(int i=0;i<thread_n;i++)
         W[i]=new G1[COMM_OPT_MAX*block_num];
+    cout<<"finish allocate mem "<<endl;
     for(int i=0;i<thread_n;i++)
         memset(W[i],0,sizeof(G1)*COMM_OPT_MAX*block_num);
     for(int i=0;i<rownum;i++) // enumerate row of T  
@@ -424,6 +355,7 @@ G1* prover_commit(ll* w, G1* g, int l,int thread_n) //compute Tk, int version wi
     }
     for (u64 i = 0; i < rownum; ++i)  //work for rownum 
         workerq.Push(i);
+    cout<<"gg in thread "<<endl;
     for(int i=0;i<thread_n;i++)
     {
         //    Tk[i]=perdersen_commit(g,row+i*colnum,colnum,W); // each thread use a different W
@@ -447,7 +379,7 @@ G1* prover_commit(ll* w, G1* g, int l,int thread_n) //compute Tk, int version wi
 
 G1* prover_commit(int* w, G1* g, int l,int thread_n) //compute Tk, int version with pippenger
 {
-    cerr<<"dog "<<thread_n<<" "<<l<<endl;
+    //cerr<<"dog "<<thread_n<<" "<<l<<endl;
     //w has 2^l length
     //assert(l%2==0);
     int halfl=l/2;
@@ -477,7 +409,7 @@ G1* prover_commit(int* w, G1* g, int l,int thread_n) //compute Tk, int version w
         tt.detach();
         t.stop("add one by one");
     }
-    t.stop("fuck!");
+    //t.stop("fuck!");
     while(!workerq.Empty())
         this_thread::sleep_for (std::chrono::microseconds(10));
     while(endq.Size()!=rownum)
@@ -493,61 +425,7 @@ G1* prover_commit(int* w, G1* g, int l,int thread_n) //compute Tk, int version w
     return Tk;
 }
 
-void ll_commit_worker(G1*& Tk,G1*& g, ll*& row,int colnum,G1*& W)
-{
-    int idx;
-    while (true)
-    {
-            bool ret=workerq.TryPop(idx);
-            if(ret==false)
-                return;
-            Tk[idx]=perdersen_commit(g,row+idx*colnum,colnum,W);
-            endq.Push(idx);
-    }
-}
-G1* prover_commit(ll* w, G1* g, int l,int thread_n) //compute Tk, int version with pippenger
-{
-    cerr<<"dog "<<thread_n<<endl;
-    //w has 2^l length
-    //assert(l%2==0);
-    int halfl=l/2;
-    int rownum=(1<<halfl),colnum=(1<<(l-halfl));
-    G1 *Tk=new G1[rownum];
-    ll* row=new ll[1<<l];
-    timer t;
-    t.start();
-    G1** W=new G1*[thread_n];
-    for(int i=0;i<thread_n;i++)
-        W[i]=new G1[COMM_OPT_MAX*block_num];
-    for(int i=0;i<thread_n;i++)
-        memset(W[i],0,sizeof(G1)*COMM_OPT_MAX*block_num);
-    for(int i=0;i<rownum;i++) // enumerate row of T  
-    {
-        for(int j=0;j<colnum;j++)// enum col
-            row[i*colnum+j]=w[i+j*rownum];
-    }
-    for (u64 i = 0; i < rownum; ++i)  //work for rownum 
-        workerq.Push(i);
-    for(int i=0;i<thread_n;i++)
-    {
-        //    Tk[i]=perdersen_commit(g,row+i*colnum,colnum,W); // each thread use a different W
-        thread t(ll_commit_worker,std::ref(Tk),std::ref(g),std::ref(row),colnum,std::ref(W[i])); 
-        t.detach();
-    }
-    while(!workerq.Empty())
-        this_thread::sleep_for (std::chrono::microseconds(10));
-    while(endq.Size()!=rownum)
-        this_thread::sleep_for (std::chrono::microseconds(10));
-    endq.Clear();
-    assert(endq.Size()==0);
-    t.stop("commit time(PPG) ");
-    for(int i=0;i<thread_n;i++)
-        delete [] W[i];
-    delete []W;
 
-    delete []row;
-    return Tk;
-}
 
 Fr prover_evaluate(Fr*w ,Fr*r,G1& G,G1* g, Fr*L,Fr*R,int l)  // nlogn brute force 
 {
@@ -556,9 +434,9 @@ Fr prover_evaluate(Fr*w ,Fr*r,G1& G,G1* g, Fr*L,Fr*R,int l)  // nlogn brute forc
     timer t(true);
     t.start();
     brute_force_compute_LR(L,R,r,l);
-    t.stop("brute force LR ",false);
+    //t.stop("brute force LR ",false);
     Fr eval=brute_force_compute_eval(w,r,l);
-    t.stop("eval ",false);
+    //t.stop("eval ",false);
     t.stop("eval total ",true,false);
     return eval;
 }
@@ -569,12 +447,12 @@ void verify(Fr*w,Fr*r,Fr eval,G1&G,G1*g,Fr*L,Fr*R,G1*tk,int l)
     int halfl=l/2;
     int rownum=(1<<halfl),colnum=(1<<(l-halfl));
     Fr* LT=new Fr[colnum];
-    timer t;
-    t.start();
+    //timer t;
+    //t.start();
     compute_LT(w,L,l,g,LT);
-    t.stop("prover compute LT ",false);
+    //t.stop("prover compute LT ",false);
     G1 tprime=compute_Tprime(w,r,l,g,L,tk);
-    t.stop("merge Tprime ",false);
+    //t.stop("merge Tprime ",false);
     prove_dot_product(tprime, G*eval, R, g , G,LT,eval,colnum);
 }
 }
